bca06ad89b0e35e11e56867cdd7cd7c967b3910b
Brayron Leiva <brayronleivas@gmail.com>
2024-03-28 10:27:45 -0600

    1.1

--- src/main/java/com/example/proyecto_01/Presentation/Clients/Controller.java
+++ src/main/java/com/example/proyecto_01/Presentation/Clients/Controller.java
@@ -4,6 +4,9 @@ import com.example.proyecto_01.logic.Clientes;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 @org.springframework.stereotype.Controller
 public class Controller {
@@ -15,9 +18,26 @@ public class Controller {
         Clientes cliente1 = new Clientes();
         cliente1.setIdCliente(1234);
         cliente1.setNombre("RANA");
+        cliente1.setClave("1111");
         model.addAttribute("cliente", cliente1);
         model.addAttribute("nombre", cliente1.getNombre()); //no es la idea
 
         return "view";
+    }
+
+    @GetMapping("/list")
+    public String list(Model model){
+        List<Clientes> list = new ArrayList<>();
+        Clientes cl1 = new Clientes();
+        cl1.setNombre("Pepe");
+        cl1.setIdCliente(1234);
+        list.add(cl1);
+        Clientes cl2 = new Clientes();
+        cl2.setNombre("Terry");
+        cl2.setIdCliente(4564);
+        list.add(cl2);
+        model.addAttribute("clients", list);
+        model.addAttribute("title", "my title");
+        return "list";
     }
 }
