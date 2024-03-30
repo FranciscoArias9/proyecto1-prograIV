6e13967d284ab22bc6342dcc1265c8a03adc45b7
Brayron Leiva <brayronleivas@gmail.com>
2024-03-29 20:45:38 -0600

    1.3 FAIL

--- src/main/java/com/example/proyecto_01/data/CientRepository.java
+++ src/main/java/com/example/proyecto_01/data/CientRepository.java
@@ -5,9 +5,10 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CientRepository extends CrudRepository<Clientes, String> {
+public interface CientRepository extends CrudRepository<Clientes, Integer> { //tipo de datos en este repositorio y tipo de llave primaria de ese elemento
 
     // Método para guardar un cliente en la base de datos
     Clientes save(Clientes cliente);
+
 }
 
